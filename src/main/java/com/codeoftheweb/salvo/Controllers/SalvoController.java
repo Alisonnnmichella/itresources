d2a9c898085000a33c6ac8b111defffe7f925d3b
alisonnnmichella <alisonnnmichella@gmail.com>
2019-10-24 11:27:14 -0300

    Tarea 5

--- src/main/java/com/codeoftheweb/salvo/Controllers/SalvoController.java
+++ src/main/java/com/codeoftheweb/salvo/Controllers/SalvoController.java
@@ -8,6 +8,7 @@ import com.codeoftheweb.salvo.Repositories.GamePlayerRepository;
 import com.codeoftheweb.salvo.Repositories.GameRepository;
 import com.codeoftheweb.salvo.Repositories.PlayerRepository;
 import com.codeoftheweb.salvo.Repositories.ShipRepository;
+import jdk.nashorn.internal.ir.RuntimeNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -48,13 +49,15 @@ public class SalvoController {
     @RequestMapping("/games")
     public Map <String,Object>  games(Authentication authentication) {
         Map <String,Object> dto= new LinkedHashMap<>();
-
-        Player player=playerRepository.findByUserName(authentication.getName());
-        if(player!=null)
-            dto.put("player",player.getDTO());
-        else
+        if(isGuest(authentication)){
             dto.put("player","Guest");
-        dto.put("games", gameRepository.findAll().stream().map(game -> game.makeCreationDTO()).collect(Collectors.toList()));
+        }
+        else{
+            Player player=playerRepository.findByUserName(authentication.getName());
+            dto.put("player",player.getDTO());
+
+        }
+         dto.put("games", gameRepository.findAll().stream().map(game -> game.makeCreationDTO()).collect(Collectors.toList()));
         return  dto;
 }
 
@@ -65,7 +68,10 @@ public class SalvoController {
 
 
     @RequestMapping("/game_view/{id}")
-    public  Map<String, Object> getGameView(@PathVariable Long id) {
+    public  Map<String, Object> getGameView(@PathVariable Long id,Authentication authentication) {
+      //  if(isGuest(authentication)){
+        //    ResponseEntity responseEntity= new ResponseEntity(hacerMap("error","Pasa algo"),HttpStatus.UNAUTHORIZED);
+        //}
         Map<String,Object> dto=new HashMap<>();
         GamePlayer gamePlayer =gamePlayerRepository.findById(id).get();
         dto.put("id",gamePlayer.getGame().getId());
@@ -76,23 +82,34 @@ public class SalvoController {
                 .map(salvo -> salvo.getDto()).collect(Collectors.toList()));
         return dto;
     }
-    @RequestMapping(path = "/players", method = RequestMethod.POST)
-    public ResponseEntity<Object> register(
-           @RequestParam String username, @RequestParam String password) {
 
-        if (username.isEmpty() || password.isEmpty() ) {
+    @RequestMapping(path = "/players", method= RequestMethod.POST)
+    public ResponseEntity<Object> register(
+           @RequestParam String email, @RequestParam String password) {
+
+        if (email.isEmpty() || password.isEmpty() ) {
             return new ResponseEntity<>("Missing data", HttpStatus.FORBIDDEN);
         }
 
-        if (playerRepository.findByUserName(username) !=  null) {
+        if (playerRepository.findByUserName(email) !=  null) {
             return new ResponseEntity<>("Name already in use", HttpStatus.FORBIDDEN);
+
         }
 
-        playerRepository.save(new Player(username, passwordEncoder.encode(password)));
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        Player player=playerRepository.save(new Player(email, passwordEncoder.encode(password)));
+//      // ResponseEntity responseEntity= new ResponseEntity(player.getDTO(),HttpStatus.CREATED);
+//        //responseEntity.getHeaders().add("url","/game_view/"+player.getId());
+//        return responseEntity;
+        return  new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     private boolean isGuest(Authentication authentication){
         return authentication==null|| authentication instanceof AnonymousAuthenticationToken;
     }
+    public Map<String,Object> hacerMap(String string,Object objeto) {
+        Map<String, Object> soyUnMapa = new LinkedHashMap<>();
+         soyUnMapa.put(string, objeto);
+         return soyUnMapa;
+    }
+
 }
--- src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
+++ src/main/java/com/codeoftheweb/salvo/SalvoApplication.java
@@ -18,7 +18,12 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
 import java.util.*;
 
@@ -348,18 +353,42 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+
                 .antMatchers("/web/**").permitAll()
-                .antMatchers("api/game_view/*").hasAnyAuthority()
+                .antMatchers("/api/game_view/*").hasAuthority("USER")
                 .antMatchers("*/h2-console/**").permitAll()
                 .antMatchers("/api/games").permitAll()
-                .antMatchers("/ap/**").hasAuthority("USER")
-                .and()
-                .formLogin()
+                .antMatchers("/api/players").permitAll()
+                .antMatchers("/api/login/").permitAll()
+                .antMatchers("/api/**").hasAuthority("USER")
+                .and().formLogin();
+                http.formLogin()
                 .usernameParameter("name")
                 .passwordParameter("pwd")
                 .loginPage("/api/login");
-        http.logout().logoutSuccessUrl("")
-        ;
 
+        http.logout().logoutUrl("/api/logout");
+
+        // turn off checking for CSRF tokens
+        http.csrf().disable();
+
+        // if user is not authenticated, just send an authentication failure response
+        http.exceptionHandling().authenticationEntryPoint((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
+
+        // if login is successful, just clear the flags asking for authentication
+        http.formLogin().successHandler((req, res, auth) -> clearAuthenticationAttributes(req));
+
+        // if login fails, just send an authentication failure response
+        http.formLogin().failureHandler((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
+
+        // if logout is successful, just send a success response
+        http.logout().logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler());
+    }
+
+    private void clearAuthenticationAttributes(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+        }
     }
 }
